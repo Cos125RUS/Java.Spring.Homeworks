cdbc4893755cfe8932cd748d68de2f9f18b07a0e
Valerii_Panov <val250640@mail.ru>
2023-12-28 22:30:52 +1000

    HW01

---
+++ Homework01/Project/src/main/java/ru/example/Person.java
@@ -0,0 +1,13 @@
+package ru.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
