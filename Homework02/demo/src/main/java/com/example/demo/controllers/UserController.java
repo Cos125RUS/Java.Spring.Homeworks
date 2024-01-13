23cb86846ee23b176213a378285a4440305d3f3c
Valerii_Panov <val250640@mail.ru>
2024-01-13 17:23:54 +1000

    fix

--- Homework02/demo/src/main/java/com/example/demo/controllers/UserController.java
+++ Homework02/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.lang.reflect.Method;
@@ -45,8 +46,8 @@ public class UserController {
     }
 
     @GetMapping("user-delete/{id}")
-    public String deleteUserCheck(User user) {
-        userService.deleteById(user.getId());
+    public String deleteUserCheck(@PathVariable("id") int id) {
+        userService.deleteById(id);
         return "redirect:/users";
     }
 
